690102e8ff61d96d4349fcc15df7b49b912fdb7d
Semphris <semphris@protonmail.com>
2021-06-15 17:39:03 -0400

    Added license (LGPLv3+)

--- src/video/window.hpp
+++ src/video/window.hpp
@@ -1,3 +1,19 @@
+//  Harbor - A portable and highly customisable game engine
+//  Copyright (C) 2021 A. Semphris <semphris@protonmail.com>
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Lesser General Public License as
+//  published by the Free Software Foundation, either version 3 of the
+//  License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public License
+//  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 #ifndef _HEADER_HARBOR_VIDEO_WINDOW_HPP
 #define _HEADER_HARBOR_VIDEO_WINDOW_HPP
 
